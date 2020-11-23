a197480309188efeca2a6cf7f4fff5e3a4610d9f
aniketwattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2020-11-23 17:10:53 +0530

    few chnges

--- main.py
+++ main.py
@@ -34,18 +34,60 @@ class Main:
         st.header('You can upload datasets and choose your algorithms and obtain a predicted file in minutes')
     
 
-#def load_data(nrows):
-#    data = pd.read_csv()
 
+@st.cache
+def load_data(file):
+    data = pd.read_csv(file)
+    
+    return data
+     
+def train_data(data):
+    training_data = data.iloc[:,:-1]
+    return training_data   
 
+def output_col(data):
+    y = data.iloc[:,-1]
+    return y
+
+def encoding(training_data):
+     
+     
+    st.write(training_data.describe())
+    st.write(training_data.select_dtypes(include = 'object'))
+#    data.select_dtypes(include = 'object')
+    
+    arr = training_data.select_dtypes(include = 'object')
+    cols = arr.columns
+    print(cols[0])
+    for col in cols:
+        training_data = pd.get_dummies(training_data, columns=[col]) 
+    st.write(training_data) 
+        
+  
+    
+
+#g_data = []
+     
+ 
 class Regression:
     
     def __init__(self):
         file = st.file_uploader('Upload Dataset')
         if file is not None:
-            data = pd.read_csv(file)
-            st.write(data)
-         
+            data = load_data(file)
+            st.write('Training Data')
+            training_data = train_data(data)
+            st.write(training_data)
+            st.write('Output Column')
+            y = output_col(data)
+            st.write(y)
+        
+        
+        if st.button('Encoding the data'):
+#            data = load_data(file)
+            encoding(training_data)
+            st.write("insdie preprocessed data")
+    
         
         
 
